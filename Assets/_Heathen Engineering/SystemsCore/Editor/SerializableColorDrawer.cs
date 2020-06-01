9a5bf1ce095770d46f61182cfad10402c4a4263d
James McGhee <jamesmcghee@heathenengineering.com>
2020-06-01 10:50:46 +0100

    2018.4.22.4
    - Added new variable types for Sprite, Texture, Image Fill Method and Image Type
    - Added new Reference types for Sprite, Texture, Image Fill Method and Image Type
    - Updated assembly def to conform to Heathen naming standard
    - re ordered script locations
    - Updated Serializable Color Drawer to support Alpha and HDR colors

--- Assets/_Heathen Engineering/SystemsCore/Editor/SerializableColorDrawer.cs
+++ Assets/_Heathen Engineering/SystemsCore/Editor/SerializableColorDrawer.cs
@@ -17,7 +17,7 @@ namespace HeathenEngineering.Scriptable.Editors
 
             EditorGUI.BeginChangeCheck();
             Color c = new Color(x.floatValue, y.floatValue, z.floatValue, w.floatValue);
-            c = EditorGUI.ColorField(position, label, c);
+            c = EditorGUI.ColorField(position, label, c, true, true, true);
             x.floatValue = c.r;
             y.floatValue = c.g;
             z.floatValue = c.b;
---
+++ Assets/_Heathen Engineering/SystemsCore/Framework/Scriptable/Data Types/Sprite/SpriteReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace HeathenEngineering.Scriptable
+{
+    [Serializable]
+    public class SpriteReference : VariableReference<Sprite>
+    {
+        public SpritePointerVariable Variable;
+        public override IDataVariable<Sprite> m_variable => Variable;
+
+        public SpriteReference(Sprite value) : base(value)
+        { }
+    }
+}
